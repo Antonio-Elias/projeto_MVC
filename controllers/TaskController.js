cb461578f6763cd35f7dc06e61796f030df793e0
Antonio Elias <aelias12j@gmail.com>
2022-12-29 10:40:17 -0300

    Atualizando status da tarefa

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -55,4 +55,16 @@ module.exports = class TaskController{
         await Task.update(task, { where: { id:id }});
         res.redirect('/tasks');
     }
+
+    static async toogleTaskStatus(req, res){
+        const id = req.body.id;
+        
+        const task = {
+            done: req.body.done === '0' ? true : false
+        };
+
+        await Task.update(task, { where: { id:id }});
+
+        res.redirect('/tasks');
+    }
 };
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -7,6 +7,7 @@ router.post('/add', TaskController.createTaskSave);
 router.post('/remove', TaskController.removeTask);
 router.get('/edit/:id', TaskController.updateTask);
 router.post('/edit', TaskController.updateTaskPost);
+router.post('/updatestatus', TaskController.toogleTaskStatus);
 router.get('/', TaskController.showTasks);
 
 
